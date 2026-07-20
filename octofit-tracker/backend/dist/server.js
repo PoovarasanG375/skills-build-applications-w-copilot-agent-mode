758fadcd3384f3c7d7195654aae1470d662b7a2d
PoovarasanG375 <poovarasan.g2@cognizant.com>
2026-07-20 06:20:54 +0000

    Add server.ts Codespaces entrypoint

---
+++ octofit-tracker/backend/dist/server.js
@@ -0,0 +1,38 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.startServer = startServer;
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const users_1 = __importDefault(require("./routes/users"));
+const teams_1 = __importDefault(require("./routes/teams"));
+const activities_1 = __importDefault(require("./routes/activities"));
+const leaderboard_1 = __importDefault(require("./routes/leaderboard"));
+const workouts_1 = __importDefault(require("./routes/workouts"));
+const database_1 = __importDefault(require("./config/database"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = Number(process.env.PORT || 8000);
+const codespaceName = process.env.CODESPACE_NAME;
+const baseUrl = codespaceName
+    ? `https://${codespaceName}-8000.app.github.dev`
+    : 'http://localhost:8000';
+app.use(express_1.default.json());
+app.get('/api/health', (_req, res) => {
+    res.json({ status: 'ok', service: 'octofit-backend', baseUrl });
+});
+app.use('/api/users', users_1.default);
+app.use('/api/teams', teams_1.default);
+app.use('/api/activities', activities_1.default);
+app.use('/api/leaderboard', leaderboard_1.default);
+app.use('/api/workouts', workouts_1.default);
+function startServer() {
+    void (0, database_1.default)();
+    app.listen(port, () => {
+        console.log(`Backend listening on port ${port}`);
+        console.log(`API base URL: ${baseUrl}`);
+    });
+}
+exports.default = app;
